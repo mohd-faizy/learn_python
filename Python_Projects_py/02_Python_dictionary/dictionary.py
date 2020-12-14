2ea1b9881fb7c8a9b4331b88f669fafe25a83b45
faizy <ifaizymohd@gmail.com>
2020-12-15 03:09:54 +0530

    update

--- Python_Projects_py/02_Python_dictionary/dictionary.py
+++ Python_Projects_py/02_Python_dictionary/dictionary.py
@@ -1,14 +1,7 @@
-# Python Dictionary
-
-# design the Interface
-# develop the word matching
-# modify the program
-
 import json
 from difflib import get_close_matches
 
-data = json.load(open('dictionary.json'))
-
+data = json.load(open("data.json"))
 
 def translate(word):
     word = word.lower()
@@ -18,25 +11,23 @@ def translate(word):
         return data[word.title()]
     elif word.upper() in data:
         return data[word.upper()]
-    elif len(get_close_matches(word, data.keys())) > 0:
-        print("did you mean %s instead" %
-              get_close_matches(word, data.keys())[0])
-        decide = input("press y for yes or n for no: ")
+    elif len(get_close_matches(word , data.keys())) > 0 :
+        print("did you mean %s instead" %get_close_matches(word, data.keys())[0])
+        decide = input("press y for yes or n for no")
         if decide == "y":
-            return data[get_close_matches(word, data.keys())[0]]
+            return data[get_close_matches(word , data.keys())[0]]
         elif decide == "n":
-            return('You have entered a worng word ')
+            return("pugger your paw steps on wrong keys ")
         else:
             return("You have entered wrong input please enter just y or n")
     else:
-        print('You have entered a worng word ')
+        print("pugger your paw steps on wrong keys")
 
 
-word = input('Enter the word you want to search: ')
+
+word = input("Enter the word you want to search")
 output = translate(word)
-
 if type(output) == list:
-
     for item in output:
         print(item)
 else:
---
+++ Python_Projects_py/03_Python_Hangman/02_hangman_C2.py
@@ -0,0 +1,132 @@
+# Code_1
+
+# Algorithm
+'''
+1. Develop the interface
+2. Predefined list
+3. instruction
+4. Check the words
+    - if right then enter in the empty list & ask for next guess
+    - if not in the list then print interface  
+5. Reduce the turn's on each iteration for every worng guess
+6. Genrate the interface
+'''
+import random
+
+
+def hangman():
+
+    word_list = random.choice(
+        ["cat", "dog", "lion", "tiger", "apple", "banana", "rat", "sheep", "firefox", "tree"])
+    valid_letters = 'abcdefghijklmnopqrstuvwxyz'
+    turns = 10
+    guessmade = ' '  # Empty string in the begining
+
+    while len(word_list) > 0:
+
+        random_word = ""
+
+        for letter in word_list:
+            if letter in guessmade:
+                random_word = random_word + letter
+            else:
+                random_word = random_word + "_" + "  "
+
+        if random_word == word_list:
+            print(random_word)
+            print("You win!")
+            break
+
+        print("Guess the word:          ", random_word)
+        guess = input()
+
+        if guess in valid_letters:
+            guessmade = guessmade + guess
+        else:
+            print("Enter a valid character")
+            guess = input()
+
+        if guess not in word_list:
+            turns = turns - 1
+            if turns == 9:
+                print("9 turns left")
+                print("   +------+   ")
+                print("          |   ")
+                print("          |   ")
+                print("          |   ")
+                print("         ===  ")
+            if turns == 8:
+                print("8 turns left")
+                print("   +------+   ")
+                print("   O      |   ")
+                print("          |   ")
+                print("          |   ")
+                print("          |   ")
+                print("         ===  ")
+            if turns == 7:
+                print("7 turns left")
+                print("   +------+   ")
+                print("   O      |   ")
+                print("   |      |   ")
+                print("          |   ")
+                print("         ===  ")
+            if turns == 6:
+                print("6 turns left")
+                print("   +------+   ")
+                print("   O      |   ")
+                print("   |      |   ")
+                print("  /       |   ")
+                print("         ===  ")
+            if turns == 5:
+                print("5 turns left")
+                print("   +------+   ")
+                print("   O      |   ")
+                print("   |      |   ")
+                print("  / \     |   ")
+                print("         ===  ")
+            if turns == 4:
+                print("4 turns left")
+                print("   +------+   ")
+                print(" \ O      |   ")
+                print("   |      |   ")
+                print("  / \     |   ")
+                print("         ===  ")
+            if turns == 3:
+                print("3 turns left")
+                print("   +------+   ")
+                print(" \ O /    |   ")
+                print("   |      |   ")
+                print("  / \     |   ")
+                print("         ===  ")
+            if turns == 2:
+                print("2 turns left")
+                print("   +------+   ")
+                print(" \ O /|   |   ")
+                print("   |      |   ")
+                print("  / \     |   ")
+                print("         ===  ")
+            if turns == 1:
+                print("1 turns left")
+                print("Last breaths counting, Take care!")
+                print("   +------+   ")
+                print(" \ O_|/   |   ")
+                print("   |      |   ")
+                print("  / \     |   ")
+                print("         ===  ")
+            if turns == 0:
+                print("You loose")
+                print("You let a kind man die")
+                print("   +------+   ")
+                print("   O _|   |   ")
+                print("  /|\     |   ")
+                print("  / \     |   ")
+                print("         ===  ")
+                break
+
+
+name = input("Enter your name: ")
+print("Welcome", name)
+print("----------------------")
+print("try to guess the word in less than 10 attempts")
+hangman()
+print()
